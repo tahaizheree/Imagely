66e765355e7786553a85410555c3d9430f9001cc
Mateen Naveed <141321018+tahaizheree@users.noreply.github.com>
2025-09-24 17:35:03 +0500

    API implementation of receiving images

---
+++ ImageManager.swift
@@ -0,0 +1,45 @@
+//
+//  ImageManager.swift
+//  Imagely
+//
+//  Created by Mateen on 9/23/25.
+//
+
+import Foundation
+
+
+protocol ImageManagerDelegate {
+    func updateUIAfterFetch()
+}
+
+class ImageManager {
+    var delegate : ImageManagerDelegate?
+    var images: [Image] = []
+
+     func fetchImages() {
+        
+        let url = URL(string: "https://picsum.photos/v2/list?page=2&limit=20")!
+        let request = URLRequest(url: url)
+
+
+        URLSession.shared.dataTask(with: request) { (data, response, error) in
+            
+            guard error == nil else { return }
+            guard let data = data else { return }
+            let decoder = JSONDecoder()
+            do {
+                let json: [Image] = try decoder.decode([Image].self, from: data)
+                self.completionOnFetch(images: json)
+                print("Images fetched")
+            } catch let error {
+                print(error.localizedDescription)
+            }
+        }.resume()
+    }
+    
+     private func completionOnFetch(images : [Image]) {
+        self.images.removeAll()
+        self.images.append(contentsOf: images)
+         delegate?.updateUIAfterFetch()
+    }
+}
